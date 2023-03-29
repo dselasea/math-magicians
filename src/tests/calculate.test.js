2357b7fe2977a9e32491ffce097320e198a98389
Daniel <dselasea@gmail.com>
2023-03-29 21:54:23 +0000

    Add test for calculate function

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,1 +1,28 @@
 import calculate from "../logic/calculate";
+
+describe('Test calculator inputs',() => {
+
+  test('total: 4, next: 5, operation null and AC', () => {
+    expect(calculate({
+      total: 4,
+      next: 5,
+      operation: null
+    }, 'AC')).toEqual({total: null, next: null, operation: null});
+  })
+
+  test('total 2, next: 15, operation: x inputs and x', () => {
+    expect(calculate({
+      total: 3,
+      next: 15,
+      operation: 'x'
+    }, 'x')).toEqual({total: "45" , next: null, operation: "x" });
+  })
+
+  test('total: 4, next: 5, operation +/- inputs and =', () => {
+    expect(calculate({
+      total: 4,
+      next: 5,
+      operation: '+/-'
+    }, '+/-')).toEqual({total: 4 , next: "-5", operation: "+/-" });
+  })
+})
