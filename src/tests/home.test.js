557a87c091cc6682f80657ccfa09a0ea4e6a8c10
veronica365 <naruth10@gmail.com>
2023-03-30 16:43:03 +0300

    Fix failing test for the navbar component

---
+++ src/tests/home.test.js
@@ -0,0 +1,8 @@
+import renderer from "react-test-renderer";
+import Home from "../components/Home";
+
+test("Test Home Component", () => {
+  const component = renderer.create(<Home />);
+  const home = component.toJSON();
+  expect(home).toMatchSnapshot();
+});
